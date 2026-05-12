c58cc402033454b0091ac09bed3e65134e4f300f
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-12 03:56:42 +0300

    feat: add register logic in user service

--- src/main/java/com/mikle/service/UserService.java
+++ src/main/java/com/mikle/service/UserService.java
@@ -1,17 +1,34 @@
 package com.mikle.service;
 
+import com.mikle.model.User;
 import com.mikle.repository.UserRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Getter
 @Setter
 @Service
 public class UserService {
-    private final UserRepository repo;
+    @Autowired
+    private final UserRepository userRepository;
+
+    private BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository repo) {
-        this.repo = repo;
+        this.userRepository = repo;
+    }
+
+    @Transactional
+    public User register(String username, String password) {
+        if (userRepository.findByUsername(username).isPresent()) throw new IllegalArgumentException();
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setPassword(passwordEncoder.encode(password));
+
+        return newUser;
     }
 }
