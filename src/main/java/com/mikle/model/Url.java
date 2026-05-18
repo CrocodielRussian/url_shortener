79656a3200195f9a037da789898c5a81f8ff5ae6
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-18 15:55:25 +0300

    fix: shortUrl is first column

--- src/main/java/com/mikle/model/Url.java
+++ src/main/java/com/mikle/model/Url.java
@@ -12,12 +12,11 @@ public class Url {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
+    private String shortUrl;
 
     @Column(unique = true, nullable = false)
     private String longUrl;
-
-    @Column(unique = true, nullable = false)
-    private String shortUrl;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
