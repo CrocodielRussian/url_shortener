734f487fca599b0c6e4d3a5e1b82a30b2f2a6acd
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-05-18 15:54:37 +0300

    fear: add request/response wrapper for url

---
+++ src/main/java/com/mikle/dto/UrlRequest.java
@@ -0,0 +1,14 @@
+package com.mikle.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class UrlRequest {
+    @NotBlank
+    private String shortUrl;
+}
---
+++ src/main/java/com/mikle/dto/UrlResponse.java
@@ -0,0 +1,12 @@
+package com.mikle.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UrlResponse {
+    private Long   id;
+    private String shortUrl;
+    private String longUrl;
+}
